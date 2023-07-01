ef3a615fb8b77ccdd900727188fda08cae86ef61
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-07-01 15:57:32 +0300

    updared handler.c file

--- handler.c
+++ handler.c
@@ -9,36 +9,80 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int len = 0;
-	int q = 0;
 
 	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
+	len = print_format(format, args);
+	va_end(args);
+	return (len);
+}
+/**
+ * print_format - format.
+ * @format: pointer.
+ * @args: argument
+ *
+ * Return: index
+ */
+int print_format(const char *format, va_list args)
+{
+	int q = 0, len = 0;
+
 	while (format && format[q] != '\0')
 	{
 		if (format[q] == '%')
-		{q++;
-			switch (format[q])
-			{case 'c': len += print_char(args); break;
-				case 's': len += print_string(args); break;
-				case '%': _putchar('%');
-					  len++; break;
-				case 'd':
-				case 'i': len += print_int(args); break;
-				case 'b': len += print_b(args); break;
-				case 'o': len += print_o(args); break;
-				case 'u': len += print_u(args); break;
-				case 'r': len += print_rev(args); break;
-				case 'x': len += print_x(args); break;
-				case 'X': len += print_X(args); break;
-				case 'p': len += print_p(args); break;
-				default: _putchar('%');
-					_putchar(format[q]);
-					len += 2; break; }
+		{
+			q++;
+			len += write_format(format, args, q);
 		}
 		else
-		{_putchar(format[q]);
-			len++; } q++; }
-	va_end(args);
+		{
+			_putchar(format[q]);
+			len++;
+		}
+		q++;
+	}
 	return (len);
+}
+/**
+ * write_format - call formats.
+ * @format: pointer.
+ * @args: arg
+ * @q: counter.
+ *
+ * Return: index.
+ */
+int write_format(const char *format, va_list args, int q)
+{
+	switch (format[q])
+	{
+		case 'c':
+			return (print_char(args));
+		case 's':
+			return (print_string(args));
+		case '%':
+			_putchar('%');
+			return (1);
+		case 'd':
+		case 'i':
+			return (print_int(args));
+		case 'b':
+			return (print_b(args));
+		case 'o':
+			return (print_o(args));
+		case 'u':
+			return (print_u(args));
+		case 'r':
+			return (print_rev(args));
+		case 'x':
+			return (print_x(args));
+		case 'X':
+			return (print_X(args));
+		case 'p':
+			return (print_p(args));
+		default:
+			_putchar('%');
+			_putchar(format[q]);
+			return (2);
+	}
 }
